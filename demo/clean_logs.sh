41c978556776b25a3bb787555d0ff257344dd1d1
scavenqqer <117581253+scavenqqer@users.noreply.github.com>
2023-07-19 11:12:42 +0800

    Delete demo directory

--- demo/clean_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -n "Removing logs and temp files... "
-sudo rm -fr logs/client/*.log
-sudo rm -fr logs/cluster/*.log
-sudo rm -fr logs/firecrest/*.log
-sudo rm -fr logs/keycloak/*.log
-sudo rm -fr minio/.minio.sys
-sudo rm -fr taskpersistence-data/dump.rdb taskpersistence-data/redis.log
-echo "done."
--- demo/source/cluster/readme.txt
+++
@@ -1,9 +0,0 @@
-Generate SSH CA keys:
-
-ssh-keygen -t rsa -b 4096 -P "" -f ca-key -C "CA Authority-test"
-
-Generate user ssh keys:
-
-ssh-keygen -t rsa -b 2048 -P "" -f user-key
-
-
