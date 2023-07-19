0af71ac3e97683e68a2a8e707ee9a4c18ae026b3
scavenqqer <117581253+scavenqqer@users.noreply.github.com>
2023-07-19 11:11:00 +0800

    Add files via upload

---
+++ demo/demo_client/config.py
@@ -0,0 +1,35 @@
+#
+#  Copyright (c) 2019-2023, ETH Zurich. All rights reserved.
+#
+#  Please, refer to the LICENSE file in the root directory.
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+class Config:
+    '''Base configuration class for the Flask app'''
+
+
+class DevConfig(Config):
+    '''Class for development configuration'''
+    DEBUG = True
+    TESTING = True
+    SECRET_KEY = 'W52ZXWUDGuUc1hq00eATyZchZL22mh0Y'
+    OIDC_CLIENT_SECRETS = 'client_secrets.json'
+    OIDC_ID_TOKEN_COOKIE_SECURE = False
+    OIDC_REQUIRE_VERIFIED_EMAIL = False
+    OIDC_USER_INFO_ENABLED = True
+    OIDC_SCOPES = ['openid', 'email', 'profile']
+    OIDC_INTROSPECTION_AUTH_METHOD = 'client_secret_post'
+    FIRECREST_IP='http://kong:8000'
+    MACHINES=['cluster']
+    PARTITIONS={'cluster':['debug','batch','serial','gpu']}
+    MICROSERVICES=['status', 'compute', 'utilities', 'storage', 'tasks']
+
+    # machine for internal storage jobs, must be defined in MACHINES
+    STORAGE_JOBS_MACHINE='cluster'
+    HOME_DIR = '/home'
+    CLIENT_PORT = 7000
+
+    # SSL configuration
+    USE_SSL = False
+    SSL_PEM = ''
+    SSL_KEY = ''
---
+++ demo/source/kong/update_kong_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##
+##  Copyright (c) 2019-2023, ETH Zurich. All rights reserved.
+##
+##  Please, refer to the LICENSE file in the root directory.
+##  SPDX-License-Identifier: BSD-3-Clause
+##
+
+# read common variables
+. ../../common/common.env
+
+# use '#' to separate string because '/' and ':' are valid on URLs
+sed -e 's#F7T_AUTH_TOKEN_ISSUER#'${F7T_AUTH_TOKEN_ISSUER}'#' \
+    -e 's#F7T_REALM_RSA_PUBLIC_KEY#'${F7T_REALM_RSA_PUBLIC_KEY}'#' \
+    -e 's#F7T_KONG_COMPUTE_URL#'${F7T_KONG_COMPUTE_URL}'#' \
+    -e 's#F7T_KONG_STATUS_URL#'${F7T_KONG_STATUS_URL}'#' \
+    -e 's#F7T_KONG_STORAGE_URL#'${F7T_KONG_STORAGE_URL}'#' \
+    -e 's#F7T_KONG_TASKS_URL#'${F7T_KONG_TASKS_URL}'#' \
+    -e 's#F7T_KONG_UTILITIES_URL#'${F7T_KONG_UTILITIES_URL}'#' \
+       < kong.yml.template > ../../kong/kong.yml
